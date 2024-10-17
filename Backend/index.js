02a38b8e8e81fb99ae2336d162063cac26c4f100
Abh002-S <abhshukla002@gmail.com>
2024-10-17 22:29:43 +0530

    Adding E-Com. Website

---
+++ Backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bookRoute from "./Route/book.route.js";
+import cors from "cors";
+import userRoute from "./Route/User.route.js";
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URL = process.env.MongoDbURL;
+
+// Connect to mongodb
+try {
+  mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("connected to mongoDb");
+} catch (error) {
+  console.log("Error: ", error);
+}
+
+//defining Routes
+
+app.use("/book", bookRoute);
+app.use("/users", userRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
