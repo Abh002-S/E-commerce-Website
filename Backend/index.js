30acdcea0fefa52689c46ba34672b081c9bb04a2
Abh002-S <abhshukla002@gmail.com>
2024-10-18 00:06:30 +0530

    fix all bug

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 import bookRoute from "./Route/book.route.js";
 import cors from "cors";
 import userRoute from "./Route/User.route.js";
+import path from "path";
 const app = express();
+
+const _dirname = path.resolve();
 
 app.use(cors());
 
@@ -29,6 +32,11 @@ try {
 
 app.use("/book", bookRoute);
 app.use("/users", userRoute);
+
+app.use(express.static(path.join(_dirname, "/Frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
