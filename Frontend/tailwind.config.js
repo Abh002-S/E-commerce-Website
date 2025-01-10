c31384d93d00e621317420a87bf9814a2fe092ba
Abh002-S <abhshukla002@gmail.com>
2025-01-10 16:35:26 +0530

    tailwaind import changes

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,12 @@
+// Importing daisyui using ES Modules syntax
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "class",
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui], // Using ES module import here
 };
